b2fbcc94f0e29f88a81cc77af799f3153b56f615
Donatas Noreika <donoras@gmail.com>
2023-08-30 08:08:34 +0300

    +Padaryti, kad užsakymo sąraše būtų matomi automobilio ir datos stulpeliai

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Automobilis, AutomobilioModelis, Uzsakymas, UzsakymoEilute, Paslauga
 
+class UzsakymasAdmin(admin.ModelAdmin):
+    list_display = ['data', 'automobilis']
+
 # Register your models here.
 admin.site.register(Automobilis)
 admin.site.register(AutomobilioModelis)
-admin.site.register(Uzsakymas)
+admin.site.register(Uzsakymas, UzsakymasAdmin)
 admin.site.register(UzsakymoEilute)
 admin.site.register(Paslauga)
