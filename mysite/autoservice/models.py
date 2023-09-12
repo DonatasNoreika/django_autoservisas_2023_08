379d3eb03d6bc62ae5564bafd5da6915fb96a58e
Donatas Noreika <donoras@gmail.com>
2023-09-12 08:27:14 +0300

    10: +Padaryti, kad prisegta profilio nuotrauka būtų automatiškai sumažinama iki norimo dydžio (pagal dizainą).

--- mysite/autoservice/models.py
+++ mysite/autoservice/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from datetime import datetime
 import pytz
 from tinymce.models import HTMLField
+from PIL import Image
 
 utc = pytz.UTC
 
@@ -113,3 +114,11 @@ class Paslauga(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(to=User, verbose_name="Vartotojas", on_delete=models.CASCADE)
     image = models.ImageField(verbose_name="Nuotrauka", upload_to="profile_pics", default="profile_pics/default.png")
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
